cde0108aaf234c8f5e03c8eee1c3a03d42545f2f
harishbob21 <harishbob8@gmail.com>
2020-12-17 22:53:37 +0530

    My Task Done

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,30 @@
+package org.emp;
+
+public class Employee {
+	
+	private void empId() {
+		System.out.println("The Employee Id is 3562");
+	}
+	private void empName() {
+		System.out.println("The Employee name is Harish");
+	}
+	private void empDob() {
+		System.out.println("The Employee Date of birth is 21st June, 1993");
+	}
+	private void empEmail() {
+		System.out.println("The Employee email id is harishbob8@gmail.com");
+	}
+	private void empAddress() {
+		System.out.println("The Employee Address is location in Chennai, TamilNadu");
+	}
+	public static void main(String[] args) {
+		Employee e=new Employee();
+		
+		e.empName();
+		e.empId();
+		e.empDob();
+		e.empEmail();
+		e.empAddress();
+	}
+
+}
