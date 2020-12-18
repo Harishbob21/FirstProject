5e13bb1d4079573383a023e988ccaacea627b5c1
harishbob21 <harishbob8@gmail.com>
2020-12-18 20:56:43 +0530

    Emp2 updated code

--- src/org/emp/Employee.java
+++ src/org/emp/Employee.java
@@ -17,6 +17,11 @@ public class Employee {
 	private void empAddress() {
 		System.out.println("The Employee Address is location in Chennai, TamilNadu");
 	}
+	
+	private void emp2Git() {
+		System.out.println("Emp2 has updated the code");
+
+	}
 	public static void main(String[] args) {
 		Employee e=new Employee();
 		
